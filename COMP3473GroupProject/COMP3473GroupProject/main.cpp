90df480cd9d4fc5973182602aa137d20f7288072
Ian Cuninghame <ijcuning@lakeheadu.ca>
2019-11-26 14:33:13 -0500

    File creation method

--- COMP3473GroupProject/COMP3473GroupProject/main.cpp
+++ COMP3473GroupProject/COMP3473GroupProject/main.cpp
@@ -27,10 +27,54 @@
 
 using namespace std;
 
-//Main Function:
+//1. Create a file method:
+FILE* createFile(char* fileName) {
 
+	if (fileName != NULL)
+		return fopen(fileName, "w");
+	else
+		return fopen("default.txt", "w");
+}
+
+//Main Function:
 int main() {
 
+	//Main function to test POFM operations
+
+	//Menu System:
+
+	bool menuOpen = true;
+
+	do {
+
+		//Menu system for command-line command interpretation
+
+
+	} while (menuOpen);
+
+	//1. Create a file:
+	
+	FILE* filePointer;
+
+	char* fileName = (char*) malloc (sizeof(char)*255);
+
+	
+
+	//2. Delete a file:
+
+	//3. Rename a file:
+
+	if (filePointer != NULL) {
+
+
+
+	}
+
+	//4. Copy a file:
+
+	//5. Move a file:
+
+	//6. Text file operations:
 
 	return 0;
 }
