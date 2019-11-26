66c7e8d50d8f44c93846628d457d7ee5afe243d8
Ian Cuninghame <ijcuning@lakeheadu.ca>
2019-11-26 14:10:17 -0500

    Added comments explaining project requirements

--- COMP3473GroupProject/COMP3473GroupProject/main.cpp
+++ COMP3473GroupProject/COMP3473GroupProject/main.cpp
@@ -1,5 +1,37 @@
 #include <stdio.h>
 #include <iostream>
 
+/*
+	COMP-3473 Operating Systems Group Project
+	Ian Cuninghame, Kris Luszczynski
+	Lakehead University 2019
+*/
+
+/*
+	POFM must perform:
+
+		1) Create a new file
+		2) Delete a file
+		3) Rename a file
+		4) Copy a file
+		5) Move a file from one directory to another
+		6) Text file operations:
+			i)   append text to the end of a file
+			ii)  insert text at a specific char position
+			iii) remove all text in a file
+			iv)  show the content of a text file, displaying a certain # of lines at once defined by the user.
+
+	- Must not use any system() calls
+
+*/
+
 using namespace std;
+
+//Main Function:
+
+int main() {
+
+
+	return 0;
+}
 
